c852619d071bb027c195f169e2de3a1c7ac0637f
Gannamaneni Akshitha <akshithag07@gmail.com>
2026-05-01 13:54:29 +0530

    Add files via upload
    Initial commit - Chat Application using Python sockets

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+nickname = input("Enter your name: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("localhost", 5555))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("utf-8")
+
+            if message == "NICK":
+                client.send(nickname.encode("utf-8"))
+            else:
+                print(message)
+
+        except:
+            print("Error!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f"{nickname}: {input('')}"
+        client.send(message.encode("utf-8"))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
